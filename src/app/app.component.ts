c96adc2dfd41a9ee4cd7ddb10f3c98ed356ee715
mpenaEVG <mauriciopenadominguez.guadalupe@alumnado.fundacionloyola.net>
2025-01-13 11:43:44 +0100

    añadido el boton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'listar-cursos';
+
+  cursos: boolean = true
+
+  handleCambio(){
+    this.cursos = !this.cursos
+  }
 }
