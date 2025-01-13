70cc9bf696077749166b19a707a200ed7ed1a220
mpenaEVG <mauriciopenadominguez.guadalupe@alumnado.fundacionloyola.net>
2025-01-13 11:57:11 +0100

    relleno de informacion

---
+++ src/app/tabla-alumnos/mock.ts
@@ -0,0 +1,22 @@
+export const alumnos = [
+  {
+    nombre: 'Ricardo',
+    apellido: 'Pérez',
+  },
+  {
+    nombre: 'Laura',
+    apellido: 'Gómez',
+  },
+  {
+    nombre: 'María',
+    apellido: 'López',
+  },
+  {
+    nombre: 'Carlos',
+    apellido: 'Ramírez',
+  },
+  {
+    nombre: 'Ana',
+    apellido: 'Martínez',
+  },
+]
---
+++ src/app/tabla-cursos/mock.ts
@@ -0,0 +1,22 @@
+export const cursos = [
+  {
+    materia: 'Matemáticas',
+    dificultad: 'Intermedia'
+  },
+  {
+    materia: 'Historia',
+    dificultad: 'Fácil'
+  },
+  {
+    materia: 'Programación',
+    dificultad: 'Avanzada'
+  },
+  {
+    materia: 'Física',
+    dificultad: 'Intermedia'
+  },
+  {
+    materia: 'Inglés',
+    dificultad: 'Fácil'
+  },
+]
