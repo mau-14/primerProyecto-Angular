bcfc86d7781947b64aa36813e1efbbe36036d027
mpenaEVG <mauriciopenadominguez.guadalupe@alumnado.fundacionloyola.net>
2025-01-13 11:39:56 +0100

    tabla alumnos y cursos

---
+++ src/app/tabla-alumnos/tabla-alumnos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-alumnos',
+  standalone: false,
+  
+  templateUrl: './tabla-alumnos.component.html',
+  styleUrl: './tabla-alumnos.component.css'
+})
+export class TablaAlumnosComponent {
+
+}
